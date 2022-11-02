71fc25b08f51c5653be1fc56a414040cac30ed21
blaze2004 <shubhamtiwari06112004@gmail.com>
2022-11-02 22:35:59 +0530

    added triggers to lambda function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -23,12 +23,6 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
-        },
-        "CustomEmailMessage": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
         }
     }
 }
